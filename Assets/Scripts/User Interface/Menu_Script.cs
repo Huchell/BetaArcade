e4433cc6abc18a91f0387705f1b0823889ee3ca0
Luke Dann <lukedann12@hotmail.co.uk>
2017-11-08 09:55:24 +0000

    UI Scripts Added

---
+++ Assets/Scripts/User Interface/Menu_Script.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu_Script : MonoBehaviour {
+
+    public Button Button;
+
+
+    void Start ()
+    {
+
+        Button btn = Button.GetComponent<Button>();
+        //btn.onClick.AddListener(OnClick);
+
+    }
+
+
+    public void OnClick ()
+    {
+
+        Debug.Log("To Level");
+        SceneManager.LoadScene("Level_Whitebox_LD");
+       
+	}
+
+    public void OnClickQuit()
+    {
+
+        Debug.Log("To Level");
+        SceneManager.LoadScene("UI_Test");
+
+    }
+
+    public void CloseGame()
+    {
+
+        Application.Quit();
+        Debug.Log("Quit");
+
+    }
+}
