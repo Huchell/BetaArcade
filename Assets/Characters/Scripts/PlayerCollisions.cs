2240a00ecc946ba0f78a0bf4ae4aca1d0905af0c
Huchell <q5059907@live.tees.ac.uk>
2017-11-22 13:16:27 +0000

    Fixed merging errors

---
+++ Assets/Characters/Scripts/PlayerCollisions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour {
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        CharacterCollisionHandler cch = hit.gameObject.GetComponent<CharacterCollisionHandler>();
+        if (cch != null)
+        {
+        }
+    }
+}
