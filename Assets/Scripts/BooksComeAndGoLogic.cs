a18203da0a8e62ab272a16a93dfd4b2bd8217542
osixjoh <06johansenad@gmail.com>
2017-12-05 20:30:22 +0000

    Platforms that show/hide books
    Super dynamic. Will produce documentation for the functionality of this
    soon.

---
+++ Assets/Scripts/BooksComeAndGoLogic.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BooksComeAndGoLogic : MonoBehaviour {
+    
+    [System.Serializable]
+    public class books
+    {
+        public GameObject prefab;
+        public bool activeWhileOn;
+    }
+
+    public List<books> bookList;
+    [HideInInspector]
+    public List<GameObject> booksInWorld = new List<GameObject>();
+    public float spawnDelay = 1.5f;
+
+    public Material ghost;
+
+    // Use this for initialization
+    void Start ()
+    {
+        for (int book = 0; book < booksInWorld.Count; book++)
+        {
+            if (bookList[book].activeWhileOn)
+            {
+                booksInWorld[book].GetComponent<MeshRenderer>().sharedMaterial = bookList[book].prefab.GetComponent<MeshRenderer>().sharedMaterial;
+                booksInWorld[book].GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(bookDelay(spawnDelay, true));
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        StartCoroutine(bookDelay(spawnDelay, false));
+    }
+    
+    public void RefreshBooks()
+    {
+        foreach (GameObject book in booksInWorld)
+        {
+            DestroyImmediate(book);
+        }
+
+        booksInWorld.Clear();
+
+        for (int x = 0; x < bookList.Count; x++)
+        {
+            if (bookList[x].prefab)
+            {
+                GameObject newBook = Instantiate(bookList[x].prefab);
+                booksInWorld.Add(newBook);
+                newBook.name += "[" + x + "]";
+
+                newBook.transform.SetParent(transform, false);
+
+                newBook.transform.position = transform.position + new Vector3(0, 0, 2f*x);
+
+                if (bookList[x].activeWhileOn)
+                {
+                    newBook.GetComponent<MeshRenderer>().sharedMaterial = ghost;
+                }
+            }
+        }
+    }
+
+    IEnumerator bookDelay(float delay, bool show)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (show)
+        {
+            for (int book = 0; book < booksInWorld.Count; book++)
+            {
+                booksInWorld[book].GetComponent<MeshRenderer>().enabled = bookList[book].activeWhileOn;
+                booksInWorld[book].GetComponent<Collider>().enabled = bookList[book].activeWhileOn;
+            }
+        }
+        else
+        {
+            for (int book = 0; book < booksInWorld.Count; book++)
+            {
+                booksInWorld[book].GetComponent<MeshRenderer>().enabled = !bookList[book].activeWhileOn;
+                booksInWorld[book].GetComponent<Collider>().enabled = !bookList[book].activeWhileOn;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Editor/BookEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BooksComeAndGoLogic))]
+public class BookEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        BooksComeAndGoLogic script = (BooksComeAndGoLogic)target;
+        if (GUILayout.Button ("Refresh Books"))
+		{
+			script.RefreshBooks();
+		}
+    }
+}
