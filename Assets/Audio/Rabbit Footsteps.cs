20e0574468431b03f776ec06adec51ab3de6bfab
rigantona <rhiannongrace@hotmail.co.uk>
2017-12-07 21:02:29 +0000

    Audio Implementation
    Implemented ambient sounds to the level, attempted to set up the start
    of the player sfx but I don't want to mess around with things I don't
    understand - will touch back on this tomorrow with Adam and or Sean :)

---
+++ Assets/Audio/Rabbit Footsteps.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitFootsteps : MonoBehaviour {
+
+	public AudioClip footSteps;
+	private AudioSource source;
+	private float volLowRange = .5f;
+	private float volHighRange = 1.0f;
+
+	// Use this for initialization
+	void Awake () {
+
+		source = GetComponent<AudioSource> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
